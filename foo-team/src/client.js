642dc18932917a275be832b39390c21b41c52be2
Konrad C <conrisc@gmail.com>
2020-01-14 11:06:26 +0100

    Includes bar-item to render on a main page

--- foo-team/src/client.js
+++ foo-team/src/client.js
@@ -1,4 +1,11 @@
-const app = document.getElementById('app');
-app.innerHTML = `
+const $app = document.getElementById('app');
+$app.innerHTML = `
     <p>This is foo-team!</p>
+    <bar-item>
+        <!--#include virtual="/bar-item" -->
+    </bar-item>
 `;
+
+// window.addEventListener('popstate', () => {
+//   rerender(window.location.pathname.substr(1));
+// });
--- foo-team/src/server.js
+++ foo-team/src/server.js
@@ -6,4 +6,13 @@ const app = express();
 app.use('/foo', express.static('dist'));
 app.get('/', express.static('dist'));
 
+app.get('/foo-app', (req, res) => {
+    res.send(`
+        <p>This is foo-team!</p>
+        <bar-item>
+            <!--#include virtual="/bar-item" -->
+        </bar-item>
+    `);
+});
+
 app.listen(port, () => { console.log(`Server is running on port ${port}`) });
